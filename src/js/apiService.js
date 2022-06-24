04e4933d72d49dd315bf5d2eb51096bb0ee3a086
Vitalina-Oleksiienko <vitalina_oleksiienko@yahoo.com>
2022-06-24 13:34:36 +0300

    Update apiService.js

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 // const BASE_URL = 'http://api.weatherstack.com';
 // const API_KEY = '65597c5b7a0295aa7818b4b27a1fb23f';
+//second
 const BASE_URL = 'https://api.weatherapi.com/v1'
 const API_KEY = 'df636f8b7f7c4cb691c131856221404';
 
